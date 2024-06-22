ceb59ae5de4c12865d3bba376b947dae5f021d84
jihyehan91 <toto5523@hanmail.net>
2024-06-22 14:12:20 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose
-  .connect(
-    'mongodb+srv://han:roqkfrhkqkf3010!!@boilerplate.bqu0hog.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate'
-  )
+  .connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/register', (req, res) => {
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  /* 
+  {
+    id: "hello",
+    password: "1234", 
+   } 
+   req.body에 위와 같은 형태로 로그인 정보가 입력되어 있음
+   -> body-parser를 이용해 클라이언트에 보내는 정보를 받아줌
+   */
+
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then((userInfo) => res.status(200).json({ success: true }))
+    .catch((err) => res.json({ success: false, err }));
 });
 
 app.listen(port, () => {
